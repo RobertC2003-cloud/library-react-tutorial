75b3a87ccf77e0281ca1d032aabd3d3fcdefedfd
Robert Dywane Crosby <robertcrosby@MacBook-Air.local>
2026-08-20 21:27:01 -0400

    updated project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
 import Nav from './components/nav';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from "./pages/Home";
-import Books from "./pages/Books";
+import Home from "./Pages/Home";
+import Books from "./Pages/Books";
+import { books } from "./data";
+import BookInfo from './Pages/BookInfo';
 
 
 
@@ -14,7 +15,8 @@ function App() {
     <div className="App">
       <Nav />
        <Route path="/" exact component={Home} />
-       <Route path="/books"  component={Books} />
+       <Route path="/books" exact render={() => <Books books={books} />} />
+       <Route path="/books1" render={() => <BookInfo books={books} />} />
       <Footer />
     </div>
     </Router>
---
+++ src/Pages/BookInfo.jsx
@@ -0,0 +1,43 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Rating from '../components/ui/Rating';
+import Price from '../components/ui/Price';
+
+const BookInfo = ({ books }) => {
+  return (
+    <div>
+      <div id="books__body">
+        <main id="books__main">
+            <div className="books__container">
+                <div className="row">
+                    <div className="book__selected--top">
+                        <Link to="/books" className="book__link">
+                        <FontAwesomeIcon icon="arrow-left" />
+                        </Link>
+                        <Link to= "/books" className="book__link">
+                        <h2 className="book__selected--title--top">Books</h2>   
+                        </Link>
+                    </div>
+                    <div className="book__selected">
+                        <figure className="book__selected--figure">
+                            <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--ig" />
+                        </figure>
+                        <div className="book__selcted--description">
+                            <h2 className="book__selected--title">Crack the coding interview</h2>
+                           <Rating rating="4.5" />
+                           <div className="book__selected--price">
+                            <Price originalPrice={50} salePrice={20} />
+                           </div>
+                           <div className="book__summary"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default BookInfo;
