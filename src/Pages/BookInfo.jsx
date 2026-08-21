c75df748de937d0eeb2d7b69ec0103b75c258730
Robert Dywane Crosby <robertcrosby@MacBook-Air.local>
2026-08-21 17:08:29 -0400

    updated project

--- src/Pages/BookInfo.jsx
+++ src/Pages/BookInfo.jsx
@@ -1,10 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link, useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import Rating from '../components/ui/Rating';
 import Price from '../components/ui/Price';
+import Book from '../components/ui/Book';
 
 const BookInfo = ({ books }) => {
+    const{ id } = useParams();
+    const book = books.find(book => +book.id === +id); 
+   
   return (
     <div>
       <div id="books__body">
@@ -21,16 +25,49 @@ const BookInfo = ({ books }) => {
                     </div>
                     <div className="book__selected">
                         <figure className="book__selected--figure">
-                            <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--ig" />
+                            <img src={book.url}
+                            alt=""
+                            className="book__selected--ig"
+                            />
                         </figure>
                         <div className="book__selcted--description">
-                            <h2 className="book__selected--title">Crack the coding interview</h2>
-                           <Rating rating="4.5" />
+                            <h2 className="book__selected--title">{book.title}</h2>
+                           <Rating rating={book.rating} />
                            <div className="book__selected--price">
-                            <Price originalPrice={50} salePrice={20} />
+                            <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
                            </div>
-                           <div className="book__summary"></div>
+                           <div className="book__summary">
+                            <h3 className="book__summary--title">
+                                Summary
+                            </h3>
+                            <p className="book__summary--para">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ipsam officia rerum ut omnis, totam error repudiandae nam a consequatur atque labore, obcaecati dolore distinctio. Architecto nesciunt autem eius eaque!
+                            </p>
+                            <p className="book__summary--para">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ipsam officia rerum ut omnis, totam error repudiandae nam a consequatur atque labore, obcaecati dolore distinctio. Architecto nesciunt autem eius eaque!
+                            </p>
+                           </div>
+                           <button className="btn">
+                            Add to cart
+                           </button>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className="books__container">
+                <div className="row">
+                    <div className="book__selected--top">
+                        <h2 className="book__selected--title--top">
+                            Recommended Books
+                        </h2>
+                    </div>
+                    <div className="books">
+                        {books
+                            .filter((book) => book.rating === 5 && +book.id !== +id)
+                            .slice(0, 4)
+                            .map((book) => (
+                            <Book book={book} key={book.id} />
+                        ))}
                     </div>
                 </div>
             </div>
--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
-import Price from '../components/ui/Price';
+import Price from './Price';
 import Rating from "./Rating";
 
 
 const Book = ({ book }) => {
   return (
    <div className="book">
-        <Link to="/books/1">
+        <Link to={`/books/${book.id}`}>
         <figure className="book__img--wrapper">
         <img src={book.url} alt="" className="book__img" />
          </figure>
         </Link>
         <div className="book__title">
-         <Link to="/books/1" className="book__title--link">
+         <Link to={`/books/${book.id}`} className="book__title--link">
          {book.title}
          </Link>
         </div>
